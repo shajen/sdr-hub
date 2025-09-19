87d3c1b75b3836d07bb52829a90099529fbca612
shajen <borysdyczko@gmail.com>
2025-09-19 19:58:03 +0200

    Added sdr scanner debug mode.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,6 +9,7 @@ echo "Running setup" &>>$LOG_FILE
 setup_broker &>>$LOG_FILE
 setup_data &>>$LOG_FILE
 setup_monitor &>>$LOG_FILE
+setup_supervisor &>>$LOG_FILE
 
 echo "Starting supervisor" | tee -a $LOG_FILE
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
