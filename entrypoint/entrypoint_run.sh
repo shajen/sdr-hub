ec8ff78bf15c001fd1f45674238d979a32e0b80d
shajen <borysdyczko@gmail.com>
2025-10-31 19:59:38 +0100

    Added id parameter to sdr-scanner.

--- entrypoint/entrypoint_run.sh
+++ entrypoint/entrypoint_run.sh
@@ -10,7 +10,9 @@ echo "Running setup" &>>$LOG_FILE
 setup_broker &>>$LOG_FILE
 setup_data &>>$LOG_FILE
 setup_monitor &>>$LOG_FILE
+setup_scanner &>>$LOG_FILE
 setup_supervisor &>>$LOG_FILE
 
+[ ! -n "$SDR_SCANNER_ID" ] && export SDR_SCANNER_ID="$(cat /var/run/sdr/sdr-scanner-id)"
 echo "Starting supervisor" | tee -a $LOG_FILE
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
